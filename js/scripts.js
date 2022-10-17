56082f4f63bc1845a2f9eb0c9423d3881fe427ad
delguanzon <yodelguanzon@gmail.com>
2022-10-17 11:34:16 -0700

    Compile results into 1 alert instead of 4
    Co-authored-by: Auston Moos <Austonhmoos2004@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ function add(number1, number2) {
 }
 
 function subtract(number1, number2) {
-let sum = number1 -number2; 
+let difference = number1 - number2; 
   return number1 - number2;
 }
 
@@ -27,13 +27,12 @@ function divide(number, number2) {
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:")); 
-const outputTextSubtract = "The substraction of your numbers equals: " + subtract(number1, number2);
-const outputTextDivide = "The division of your numbers equals: " + divide(number1, number2);
-const outputTextMultiply = "The multiplication of your numbers equals: " + multiply(number1, number2);
-const outputTextAdd = "The addition of your numbers equals: " + add(number1, number2);
+const outputTextSubtract = number1.toString() + "-" + number2.toString() + "=" + subtract(number1, number2);
+const outputTextDivide = number1.toString() + "/" + number2.toString() + "="  + divide(number1, number2);
+const outputTextMultiply = number1.toString() + "*" + number2.toString() + "="  + multiply(number1, number2);
+const outputTextAdd = number1.toString() + "+" + number2.toString() + "="  + add(number1, number2);
 
-window.alert(outputTextAdd);
-window.alert(outputTextDivide);
+window.alert(outputTextAdd + "." + "\n" + outputTextSubtract + "." + "\n" + outputTextMultiply + "." + "\n" + outputTextDivide +".");
+/*window.alert(outputTextDivide);
 window.alert(outputTextSubtract);
-window.alert(outputTextMultiply);
-                    // this line is new
+window.alert(outputTextMultiply);*/
